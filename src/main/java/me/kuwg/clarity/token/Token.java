487451b8642b9359a91fecce97c06ea743105b39
Kuwg <endahpvp@gmail.com>
2024-08-16 00:28:02 +0200

    Added TokenType class and implemented Token

--- src/main/java/me/kuwg/clarity/token/Token.java
+++ src/main/java/me/kuwg/clarity/token/Token.java
@@ -1,5 +1,25 @@
 package me.kuwg.clarity.token;
 
-public class Token {
+public final class Token {
+    private final TokenType type;
+    private final String value;
+    private final int line;
 
+    public Token(final TokenType type, final String value, final int line) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getLine() {
+        return line;
+    }
 }
---
+++ src/main/java/me/kuwg/clarity/token/TokenType.java
@@ -0,0 +1,7 @@
+package me.kuwg.clarity.token;
+
+public enum TokenType {
+
+
+
+}
