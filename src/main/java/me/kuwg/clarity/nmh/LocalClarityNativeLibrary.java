0239e8f636eb0e16cf18bda363237278b58faa60
Kuwg <endahpvp@gmail.com>
2024-09-23 20:52:16 +0200

    Added JavaDOC to LocalClarityNativeLibrary

--- src/main/java/me/kuwg/clarity/nmh/LocalClarityNativeLibrary.java
+++ src/main/java/me/kuwg/clarity/nmh/LocalClarityNativeLibrary.java
@@ -8,34 +8,52 @@ import me.kuwg.clarity.nmh.natives.impl.clazz.MathNativeClass;
 import me.kuwg.clarity.nmh.natives.impl.clazz.ReflectionsNativeClass;
 import me.kuwg.clarity.nmh.natives.impl.def.*;
 
+/**
+ * The {@code LocalClarityNativeLibrary} class provides a set of native functions and classes
+ * for the Clarity interpreter. This class serves as a local implementation of the
+ * {@link ClarityNativeLibrary} interface, encapsulating various utility functions and
+ * classes that can be utilized within the interpreter.
+ */
 public class LocalClarityNativeLibrary implements ClarityNativeLibrary {
 
+    /**
+     * Constructs a new instance of {@code LocalClarityNativeLibrary}.
+     * The constructor is protected to restrict instantiation from outside the package.
+     */
     protected LocalClarityNativeLibrary() {
     }
 
+    /**
+     * Returns an array of native functions provided by this library.
+     *
+     * @return an array of {@link ClarityNativeFunction} instances that are available in this library
+     */
     @Override
     public ClarityNativeFunction<?>[] getLibraryNativeFunctions() {
         return new ClarityNativeFunction[] {
-            new PrintlnNative(),
-            new InputNative(),
-            new PrintNative(),
-            new NowNative(),
-            new NanoNative(),
-            new ErrNative(),
-            new ExecNative(),
-            new SleepNative(),
-            new TypeOfNative(),
-
+                new PrintlnNative(),
+                new InputNative(),
+                new PrintNative(),
+                new NowNative(),
+                new NanoNative(),
+                new ErrNative(),
+                new ExecNative(),
+                new SleepNative(),
+                new TypeOfNative(),
         };
     }
 
+    /**
+     * Returns an array of native classes provided by this library.
+     *
+     * @return an array of {@link ClarityNativeClass} instances that are available in this library
+     */
     @Override
     public ClarityNativeClass[] getLibraryNativeClasses() {
         return new ClarityNativeClass[] {
                 new MathNativeClass(),
                 new ReflectionsNativeClass(),
                 new FileNativeClass(),
-
         };
     }
 }
